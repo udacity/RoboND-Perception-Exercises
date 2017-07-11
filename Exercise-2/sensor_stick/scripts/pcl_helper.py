844f5ba6ded8a10ee62257284b773e17865279f1
Harsh Pandya <hvpandya@users.noreply.github.com>
2017-07-11 10:52:11 -0700

    Added float_to_rgb

--- Exercise-2/sensor_stick/scripts/pcl_helper.py
+++ Exercise-2/sensor_stick/scripts/pcl_helper.py
@@ -147,13 +147,7 @@ def XYZ_to_XYZRGB(XYZ_cloud, color):
     XYZRGB_cloud = pcl.PointCloud_PointXYZRGB()
     points_list = []
 
-    hex_r = (0xff & color[0]) << 16
-    hex_g = (0xff & color[1]) << 8
-    hex_b = (0xff & color[2])
-
-    hex_rgb = hex_r | hex_g | hex_b
-
-    float_rgb = struct.unpack('f', struct.pack('i', hex_rgb))[0]
+    float_rgb = rgb_to_float(color)
 
     for data in XYZ_cloud:
         points_list.append([data[0], data[1], data[2], float_rgb])
@@ -173,7 +167,7 @@ def rgb_to_float(color):
             color (list): 3-element list of integers [0-255,0-255,0-255]
             
         Returns:
-            float: RGB value packed as a float
+            float_rgb: RGB value packed as a float
     """
     hex_r = (0xff & color[0]) << 16
     hex_g = (0xff & color[1]) << 8
@@ -186,6 +180,28 @@ def rgb_to_float(color):
     return float_rgb
 
 
+def float_to_rgb(float_rgb):
+    """ Converts a packed float RGB format to an RGB list    
+        
+        Args:
+            float_rgb: RGB value packed as a float
+            
+        Returns:
+            color (list): 3-element list of integers [0-255,0-255,0-255]
+    """
+    s = struct.pack('>f', float_rgb)
+	i = struct.unpack('>l', s)[0]
+	pack = ctypes.c_uint32(i).value
+			
+	r = (pack & 0x00FF0000) >> 16
+	g = (pack & 0x0000FF00) >> 8
+	b = (pack & 0x000000FF)
+			
+	color = [r,g,b]
+			
+	return color
+
+            
 def get_color_list(cluster_count):
     """ Returns a list of randomized colors
     
